775c2fd09b31d958d5f10a1dd381959cb68f1644
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2017-01-19 02:23:48 -0300

    mat_inverse.m code beautify

--- mat_inverse.m
+++ mat_inverse.m
@@ -1,4 +1,4 @@
-function [niter,vr_iter,vp_iter,vs_iter,dns_iter] = mat_inverse(freq,vr_exp,sigma,thk,vp,vs,dns,maxiter, mu,tol_vs)
+function [niter, vr_iter, vp_iter, vs_iter, dns_iter] = mat_inverse(freq, vr_exp, sigma, thk, vp, vs, dns, maxiter, mu, tol_vs)
 % input:
 %   1. dispersion curve
 %       freq, vr_exp, sigma
@@ -14,12 +14,12 @@ function [niter,vr_iter,vp_iter,vs_iter,dns_iter] = mat_inverse(freq,vr_exp,sigm
 nl = length(thk);
 
 % Weight matrix
-w = diag(1./sigma);
+w = diag(1 ./ sigma);
 
 % Second derivative
-delta = curv(nl,nl+1);
-L = delta; 
-rms = zeros(maxiter,1);
+delta = curv(nl, nl + 1);
+L = delta;
+rms = zeros(maxiter, 1);
 
 % Initialize intial guess
 m0 = vs;
@@ -28,52 +28,52 @@ vs0 = vs;
 dns0 = dns;
 
 % Initialize interation variables
-vp_iter = zeros(nl+1,maxiter);
-vs_iter = zeros(nl+1,maxiter);
-dns_iter= zeros(nl+1,maxiter);
-vr_iter = zeros(length(freq),maxiter);
+vp_iter = zeros(nl + 1, maxiter);
+vs_iter = zeros(nl + 1, maxiter);
+dns_iter = zeros(nl + 1, maxiter);
+vr_iter = zeros(length(freq), maxiter);
 
-for i=1:maxiter
-
+for i = 1:maxiter
+ 
     % Calculate theoretical phase velocity and partial derivatives
     % warning: presently the code only handle 1 type of dispersion!
-    [vr,dvrvs,~] = mat_disperse(thk,dns0,vp0,vs0,freq);
-    jac = real(squeeze(dvrvs)); %jac = [real(squeeze(dvrvs)) real(squeeze(dvrrho))];
-
+    [vr, dvrvs, ~] = mat_disperse(thk, dns0, vp0, vs0, freq);
+    jac = real(squeeze(dvrvs)); % jac = [real(squeeze(dvrvs)) real(squeeze(dvrrho))];
+ 
     % Calculate the rms error
-    error  = w*(vr-vr_exp);
-    rms(i) = sqrt( mean(error.^2) );
-
+    error = w * (vr - vr_exp);
+    rms(i) = sqrt(mean(error .^ 2));
+ 
     % Least square inversion
     wjac = w * jac;
-
-    b  = w * (vr_exp - vr + jac * m0);
-    m1 = (wjac'*wjac + mu^2*(L'*L))\(wjac'*b);     
-
+ 
+    b = w * (vr_exp - vr + jac * m0);
+    m1 = (wjac'*wjac + mu^2*(L' * L)) \ (wjac'*b);
+ 
     % Evaluate new model
-    vs1 = m1(1:nl+1);
+    vs1 = m1(1:nl + 1);
     vp1 = vp;
-    dns1= dns;  %dns1 = m1(nl+2:nl+2+nl);
-    vr = mat_disperse(thk,dns1,vp1,vs1,freq);
-    error1 = w*(vr-vr_exp);
-    rms1   = sqrt( mean(error1.^2) );
-
+    dns1 = dns; % dns1 = m1(nl+2:nl+2+nl);
+    vr = mat_disperse(thk, dns1, vp1, vs1, freq);
+    error1 = w * (vr - vr_exp);
+    rms1 = sqrt(mean(error1 .^ 2));
+ 
     % Store the models
-    vp_iter(:,i) = vp1;
-    vs_iter(:,i) = vs1;
-    dns_iter(:,i)= dns1;
-    vr_iter(:,i) = vr(:);
-
+    vp_iter(:, i) = vp1;
+    vs_iter(:, i) = vs1;
+    dns_iter(:, i) = dns1;
+    vr_iter(:, i) = vr(:);
+ 
     % Check for convergence, only check vs ?
     diff_vs = vs1 - vs0;
-    rms_vs_change = sqrt(mean(diff_vs.^2));
-
-    if rms_vs_change<tol_vs || rms1 <= 1 
+    rms_vs_change = sqrt(mean(diff_vs .^ 2));
+ 
+    if rms_vs_change < tol_vs || rms1 <= 1
         break
     end
-
-    m0  = m1;
-    dns0= dns1;
+ 
+    m0 = m1;
+    dns0 = dns1;
     vp0 = vp1;
     vs0 = vs1;
 end
@@ -81,10 +81,10 @@ niter = i;
 end
 
 % Curvature matrix for regularization
-function delta = curv(m,~)
-    delta   = diag( ones(1,m),0 ) + diag(-2*ones(1,m-1), 1 ) + diag(ones(1,m-2),2);
-    tmp   = zeros(m,1);
-    tmp(m)  = -1; 
-    tmp(m-1)=  1; 
-    delta   = [delta tmp];
+function delta = curv(m, ~)
+delta = diag(ones(1, m), 0) + diag(- 2 * ones(1, m - 1), 1) + diag(ones(1, m - 2), 2);
+tmp = zeros(m, 1);
+tmp(m) = - 1;
+tmp(m - 1) = 1;
+delta = [delta tmp];
 end
