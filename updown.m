314bfa9efaefc542f32e38d6ffc3fad2ee309e71
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2017-01-19 02:03:56 -0300

    updown code beautify

--- updown.m
+++ updown.m
@@ -1,4 +1,4 @@
-function [lamd,lamu] = updown(thk,cvp,cvs,om,k,z,layer)
+function [lamd, lamu] = updown(thk, cvp, cvs, om, k, z, layer)
 % This function calculates the up-going and down-going matrices for the P-SV case
 % Note that the function psv also calculates up-going and down-going matrices
 % (called du) which are optimized for use in calculating the modified R/T
@@ -21,30 +21,30 @@ function [lamd,lamu] = updown(thk,cvp,cvs,om,k,z,layer)
 % along with this program; if not, write to the Free Software
 % Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-cvs2 = cvs(layer).^2; cvp2 = cvp(layer).^2;
-depth = [0 ; cumsum(thk)];
+cvs2 = cvs(layer) .^ 2; cvp2 = cvp(layer) .^ 2;
+depth = [0; cumsum(thk)];
 
-lamd = zeros(2,2);
-lamu = zeros(2,2);
+lamd = zeros(2, 2);
+lamu = zeros(2, 2);
 
-k2 = k^2; om2 = om^2;
-   
-ks2 = om2/cvs2;
-nus = sqrt(k2-ks2);
-if imag(-i*nus) > 0 %#ok<*IJCL>
-    nus = -nus;
-end   
-   
-kp2 = om2/cvp2;
-nup = sqrt(k2-kp2);
-if imag(-i*nup) > 0
-    nup = -nup;
-end   
-   
-lamd(1,1) = exp(-nup*(z-depth(layer)));
-lamd(2,2) = exp(-nus*(z-depth(layer)));
-   
+k2 = k ^ 2; om2 = om ^ 2;
+
+ks2 = om2 / cvs2;
+nus = sqrt(k2 - ks2);
+if imag(- i * nus) > 0 % #ok<*IJCL>
+    nus = - nus;
+end
+
+kp2 = om2 / cvp2;
+nup = sqrt(k2 - kp2);
+if imag(- i * nup) > 0
+    nup = - nup;
+end
+
+lamd(1, 1) = exp(- nup * (z - depth(layer)));
+lamd(2, 2) = exp(- nus * (z - depth(layer)));
+
 if layer <= length(thk)
-    lamu(1,1) = exp(-nup*(depth(layer+1)-z));
-    lamu(2,2) = exp(-nus*(depth(layer+1)-z));
+    lamu(1, 1) = exp(- nup * (depth(layer + 1) - z));
+    lamu(2, 2) = exp(- nus * (depth(layer + 1) - z));
 end
