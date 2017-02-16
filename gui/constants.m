e3f0f9b34954ff811619108eeffadff7b359b6e2
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2017-02-16 00:31:55 -0300

    Version 2.12

--- gui/constants.m
+++ gui/constants.m
@@ -18,7 +18,7 @@
 % Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 % Version
-software_version = '2.11';
+software_version = '2.12';
 
 % Total of avaiable languages (English, French, Spanish)
 lang_avaiable_languages = 3;
